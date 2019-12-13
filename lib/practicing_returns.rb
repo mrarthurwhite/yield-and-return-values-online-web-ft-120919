f0c481e0fa85ada851b62a98d93ece8b387f73a6
Arthur White <mister.arthur.white@gmail.com>
2019-12-13 04:37:55 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,11 +4,11 @@ def hello(array)
   i = 0
   while i < array.length
 
-  array << yield(array[i])
+  a << yield(array[i])
     i += 1
   end
-  puts array.length
-  puts array
+  puts a.length
+  puts a
 end
 
 
